fe0faf176832017ce59eee854a13dfcddd6dd6af
Paper Lee <paper@diing.com>
2021-07-27 13:51:05 -0700

    Dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -17,17 +17,18 @@ function Page() {
   return (
     <section>
       <article className="container">
-        <header className="row">
-          <h2>Select a Mode</h2>
+        <header className="row instruction">
+          <h2>Select a Scan Media</h2>
+          <p>Choose what kind of media you are going to scan, someone wearing DiingT Vol. 5 or the card.</p>
         </header>
         <ul className="buttons row d-flex flex-row">
           <li>
-            <a rel="ar" className="button-ar" href="/dt5/dt5_t.usdz#custom=https://shop.diing.com&customHeight=small">
+            <a rel="ar" className="button-ar" id="button-ar-tshirt" href="/dt5/dt5_t.usdz#custom=https://shop.diing.com&customHeight=small">
               <img src={tshirt} alt="DiingT Vol.5 on T-Shirt" />
             </a>
           </li>
           <li>
-            <a rel="ar" className="button-ar" href="/dt5/dt5_c.usdz#custom=https://shop.diing.com&customHeight=small">
+            <a rel="ar" className="button-ar" id="button-ar-card" href="/dt5/dt5_c.usdz#custom=https://shop.diing.com&customHeight=small">
               <img src={card} alt="DiingT Vol.5 on Card" />
             </a>
           </li>
